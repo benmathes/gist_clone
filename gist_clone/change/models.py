1bb20b4cc5b410b6b53a7e5e5351aa68b76a56eb
Ben Mathes <mathes.ben@gmail.com>
2019-05-16 20:12:54 -0700

    file versions

--- gist_clone/change/models.py
+++ gist_clone/change/models.py
@@ -3,5 +3,9 @@ import uuid
 
 
 class Change(models.Model):
+    """
+    If I decide to implement storing just diffs for space saving, this model
+    will be used.
+    """
     created_at = models.DateTimeField(auto_now_add=True)
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- gist_clone/file/models.py
+++ gist_clone/file/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
+from gist_clone.user.models import User
+
 
 class File(models.Model):
+    name = models.TextField()
     last_modified = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class FileVersion(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    file = models.ForeignKey(File, on_delete=models.CASCADE, related_name='versions')
+    created_by = models.ForeignKey(User, related_name='versions',
+                                   null=True, on_delete=models.SET_NULL)
